eccf28d49ba822becf2a39c6e3b814b8e67a20b0
Michael Smethurst <michael@smethur.st>
2022-11-21 12:27:34 +0000

    removed elections query from show method

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -8,18 +8,6 @@ class PartyController < ApplicationController
   def show
     party = params[:party]
     @party = Party.find( party )
-    @elections = Election.find_by_sql(
-      "
-        SELECT e.*, y.year AS year, ge.name AS general_election_name, con.name AS constituency_name
-        FROM years y, general_elections ge, elections e, candidacies c, constituencies con
-        WHERE y.id = ge.year_id
-        AND ge.id = e.general_election_id
-        AND e.id = c.election_id
-        AND c.party_id = #{@party.id}
-        AND e.constituency_id = con.id
-        ORDER BY y.year, ge.id, con.name
-      "
-    )
     @title = @party.name
   end
 end
