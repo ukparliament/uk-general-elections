fc12834b3d2220f73b16163d13101cf0d3ce9317
fantasticlife <michael@smethur.st:wq>
2024-04-17 14:15:10 +0100

    testing blocking

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -26,6 +26,8 @@ Rack::Attack.safelist_ip('162.158.78.106')
 Rack::Attack.safelist_ip('162.158.79.231')
 Rack::Attack.safelist_ip('172.71.242.77')
 Rack::Attack.safelist_ip('172.69.194.216')
+Rack::Attack.safelist_ip('162.158.78.31')
+
 
 
 
