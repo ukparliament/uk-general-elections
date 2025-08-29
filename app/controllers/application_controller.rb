cfd7a820a1afc4e0a4411d278846d46d002b4e0d
fantasticlife <michael@smethur.st>
2025-08-29 14:30:33 +0100

    added site title and crumb constructor to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   
+  include LibraryDesign::Crumbs
+
+  $SITE_TITLE = 'UK General Elections Results 1832 - 2019'
+  
   before_action do
     expires_in 3.minutes, :public => true
   end
