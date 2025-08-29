45e2756dc7d57d85d79b2060ddebcd0a47b8f4eb
fantasticlife <michael@smethur.st>
2025-08-29 14:26:37 +0100

    added mounting of design system to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  mount LibraryDesign::Engine => "/library_design"
 
   # Defines the root path route ("/")
 
