2b88d62fe49b175650f30ab2da6895df23a8dfa0
fantasticlife <michael@smethur.st>
2025-08-30 08:39:57 +0100

    added page meta stuff to controller actions

--- app/controllers/candidate_name_controller.rb
+++ app/controllers/candidate_name_controller.rb
@@ -1,7 +1,6 @@
 class CandidateNameController < ApplicationController
   
   def index
-    @title = 'Candidate names'
     @candidate_names = CandidateName.find_by_sql(
       "
         SELECT cn.*
@@ -9,11 +8,21 @@ class CandidateNameController < ApplicationController
         ORDER BY cn.name
       "
     )
+    @page_title = 'Candidate names'
+    @description = 'Candidate names.'
+    @section = 'candidates'
+    @crumb << { label: @page_title, url: nil }
   end
   
   def show
     candidate_name = params[:candidate_name]
     @candidate_name = CandidateName.find( candidate_name )
-    @title = "Candidate names - #{@candidate_name.name}"
+    @page_title = "Candidate names - #{@candidate_name.name}"
+    @description = "Candidates with the name #{@candidate_name.name}."
+    @section = 'candidates'
+    @crumb << {label: 'Candidate names', url: letter_list_url}
+    @crumb << {label: @candidate_name.surname.surname[0,1], url: letter_show_url( :letter => @candidate_name.surname.letter )}
+    @crumb << {label: @candidate_name.surname.surname, url: surname_show_url( :surname => @candidate_name.surname )}
+    @crumb << {label: @candidate_name.name, url: nil}
   end
 end
