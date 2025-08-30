fff49443299ea86321c12f4f1aa2d92a4b316157
fantasticlife <michael@smethur.st>
2025-08-30 08:42:33 +0100

    added page meta stuff to controller actions

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -3,11 +3,18 @@ class PartyController < ApplicationController
   def index
     @parties = Party.all.order( 'name' )
     @page_title = 'Parties'
+    @description = 'Parties.'
+    @section = 'parties'
+    @crumb << { label: @page_title, url: nil }
   end
   
   def show
     party = params[:party]
     @party = Party.find( party )
-    @title = @party.name
+    @page_title = @party.name
+    @description = "#{@party.name}."
+    @section = 'parties'
+    @crumb << { label: 'Parties', url: party_list_url }
+    @crumb << { label: @page_title, url: nil }
   end
 end
